fe8149dda1e05096ddb75f1c265a45e4fd456651
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2025-12-12 17:12:01 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,24 @@
 using namespace std;
 
 char before(char x){
+   
     if (x >= 'A' && x <= 'Z') {
+        
+  
         if (x == 'A') {
             return 'Z';
         } else {
+      
             return x - 1;
         }
     } else {
+ 
         return '0';
     }
 }
 
 int main(){
+ 
     cout << before('A') << "\n";
     cout << before('B') << "\n";
     cout << before('P') << "\n";
